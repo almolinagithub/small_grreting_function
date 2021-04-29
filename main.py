3b0669d1c6ff47a1516f48eb43a0cc43ed5e5b24
Alessandro Molinari <alessandromolinari2000@hotmail.com>
2021-04-29 15:27:41 +0000

    Third function added

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ def greet():
   print("Howdy")
 
 greet()
+
+def greet_w_name(name):
+    print("Hi there "+name)
+    print("Ciao "+name)
+    print("Howdy" + name)
+
+greet_w_name("Alex") 
+
+def greet_with(name,location):
+  print("ciao "+ name +"vai pure a "+ location)
+
+greet_with("Alex ","CAlcutta")
